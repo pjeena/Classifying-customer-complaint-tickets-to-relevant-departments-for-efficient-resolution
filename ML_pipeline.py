392e14d9c9b2134632b4d022c47534a74a0c34a5
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-06-27 08:50:19 +0200

    Update ML_pipeline.py

--- ML_pipeline.py
+++ ML_pipeline.py
@@ -8,7 +8,6 @@ from src.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeli
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-#   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    print(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
@@ -43,7 +42,6 @@ except Exception as e:
 
 STAGE_NAME = "Model Trainer stage"
 try: 
-#   logger.info(f"*******************")
    print(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainerTrainingPipeline()
    model_trainer.main()
@@ -57,7 +55,6 @@ except Exception as e:
 
 STAGE_NAME = "Model Evaluation stage"
 try: 
-#   logger.info(f"*******************")
    print(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_evaluation = ModelEvaluationTrainingPipeline()
    model_evaluation.main()
